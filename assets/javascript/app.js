140d2dc4f70a68e46248a38be9dbc5e8b3498ac7
Devyn Barron <52061626+barrd001@users.noreply.github.com>
2019-09-02 10:16:51 -0700

    buttons and search working

---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+var videoGames = ["devil may cry", "final fantasy", "lego"];
+
+function renderButtons() {
+    $('#button-view').empty();
+    for (var i = 0; i < videoGames.length; i++) {
+        var gameButton = $('<button>', {
+        text: videoGames[i],
+        id: videoGames[i],
+        // create class ID for play/pause click event
+        class: 'gif-button',
+        'data-name': videoGames[i],
+        // initial data state for play/pause click event
+        'data-state': 'still',
+        // define still and animated url's for play/pause click event
+        // 'data-still': 
+        // 'data-animate': 
+    });
+    $('#button-view').append(gameButton);
+};
+};
+
+$('#add-gif').on('click', function(event) {
+    event.preventDefault();
+
+    var game = $('#search-input').val().trim();
+
+    videoGames.push(game);
+
+    renderButtons();
+ 
+});
+
+$('.gif-button').on('click', function() {
+    
+    var key = 'QJwZn4MuWdgdO9uOc1Sey3flUREZtjYx';
+    var gameGif = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        gameGif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    console.log(gameGif);
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    .then(function(response) {
+        var results = response.data;
+
+        console.log(response);
+        for(var j = 0; j <  results.length; j++) {
+            var gifDiv = $('<div>');
+
+            gifDiv.addClass('gif');
+
+            var rating = results[j].rating;
+
+            var p = $('<p>').text('Rating: ' +  rating);
+
+            var renderGif = $('<img>');
+            renderGif.attr('src', results[j].images.fixed_height.url);
+
+            gifDiv.append(p);
+            gifDiv.append(renderGif);
+
+            $('#gif-view').append(gifDiv);
+        }
+    })
+})
+renderButtons();
+console.log(videoGames);
